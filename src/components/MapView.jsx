7fedba9da7fe283e2ecf478191ff8d8c24adcfe6
Baza Raphael <Bamakakidi@gmail.com>
2026-09-25 14:38:38 +0100

    fix(react): import missing useMemo in MapView

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useMemo } from 'react';
 import { MapContainer, TileLayer, Polygon, Polyline, Popup, Tooltip, useMap, Marker, useMapEvents, Circle } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
